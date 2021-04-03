057183f239e4c4fd47bca3f4cda4b8ea5d396c1a
Gvieve <gnuebel13@gmail.com>
2021-04-02 18:28:30 -0600

    Create model tests and associations

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it {should belong_to :item}
+    it {should belong_to :invoice}
+    it {should have_many(:transactions).through(:invoice)}
+    it {should have_one(:merchant).through(:item)}
+    it {should have_one(:customer).through(:invoice)}
+  end
 end
