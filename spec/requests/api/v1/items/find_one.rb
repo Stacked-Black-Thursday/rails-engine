271567d7f2f025cd2880abd6fccbbc0de7e82ca1
Gvieve <gnuebel13@gmail.com>
2021-04-05 22:12:08 -0600

    Add tests and methods for min/max price on items/search#show

--- spec/requests/api/v1/items/find_one.rb
+++ spec/requests/api/v1/items/find_one.rb
@@ -43,4 +43,116 @@ describe 'find a single item' do
       expect(json[:data]).to be_a(Hash)
     end
   end
+
+  describe 'happy path' do
+    it "find one item by min price" do
+      item1 = create(:item, name: "B Item", unit_price: 30.99)
+      item2 = create(:item, name: "A Item", unit_price: 20.99)
+      item3 = create(:item, name: "C Item", unit_price: 10.99)
+      min_price = 18.99
+
+      get "/api/v1/items/find?min_price=#{min_price}"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(json.count).to eq(1)
+      expect(json[:data][:id]).to_not eq(item1.id.to_s)
+      expect(json[:data][:id]).to eq(item2.id.to_s)
+      expect(json[:data][:attributes][:name]).to eq(item2.name)
+    end
+
+    it "fetches one item by max price" do
+      item1 = create(:item, name: "B Item", unit_price: 100.99)
+      item2 = create(:item, name: "A Item", unit_price: 99.99)
+      item3 = create(:item, name: "C Item", unit_price: 300.99)
+      max_price = 200.00
+
+      get "/api/v1/items/find?max_price=#{max_price}"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(json.count).to eq(1)
+      expect(json[:data][:id]).to_not eq(item1.id.to_s)
+      expect(json[:data][:id]).to eq(item2.id.to_s)
+      expect(json[:data][:attributes][:name]).to eq(item2.name)
+    end
+
+    it "returns 200 but no data if min price is too big" do
+      item1 = create(:item, name: "B Item", unit_price: 100.99)
+      item2 = create(:item, name: "A Item", unit_price: 99.99)
+      item3 = create(:item, name: "C Item", unit_price: 300.99)
+
+      min_price = 1000.00
+
+      get "/api/v1/items/find?min_price=#{min_price}"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(json.count).to eq(1)
+      expect(json[:data]).to be_a(Hash)
+      expect(json[:data].empty?).to eq(true)
+    end
+
+    it "returns 200 but no data if max price is too small" do
+      item1 = create(:item, name: "B Item", unit_price: 100.99)
+      item2 = create(:item, name: "A Item", unit_price: 99.99)
+      item3 = create(:item, name: "C Item", unit_price: 300.99)
+
+      max_price = 1.00
+
+      get "/api/v1/items/find?max_price=#{max_price}"
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(json.count).to eq(1)
+      expect(json[:data]).to be_a(Hash)
+      expect(json[:data].empty?).to eq(true)
+    end
+  end
+
+
+  describe 'sad path' do
+    it "returns 400 if it sends name and min_price" do
+      get "/api/v1/items/find?name=nonos&min_price=100"
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(400)
+    end
+
+    it "returns 400 if it sends name and max_price" do
+      get "/api/v1/items/find?name=nonos&max_price=100"
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(400)
+    end
+
+    it "returns 400 if max_price is less than min_price" do
+      get "/api/v1/items/find?min_price=100&max_price=10"
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(400)
+    end
+
+    it "returns 400 cannot have a min price below 0" do
+      get "/api/v1/items/find?min_price=-1"
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(400)
+    end
+
+    it "returns 400 if max price is less than 0" do
+      get "/api/v1/items/find?max_price=-1"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+    end
+  end
 end
